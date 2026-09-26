d2adfed57b9a8a55e550a16b9791a4f728bb35b5
AlexgDevs <asmetanin014@gmail.com>
2026-09-26 16:57:34 +0500

    feat: add postgres database layer

---
+++ backend/src/core/database/__init__.py
@@ -0,0 +1,21 @@
+from src.core.database.mixin import UUIDMixin
+from src.core.database.types import (
+    int_pk,
+    str_128,
+    str_255,
+    str_512,
+    str_1024,
+    timestamp,
+    uuid_pk,
+)
+
+__all__ = [
+    "UUIDMixin",
+    "int_pk",
+    "str_128",
+    "str_255",
+    "str_512",
+    "str_1024",
+    "timestamp",
+    "uuid_pk",
+]
